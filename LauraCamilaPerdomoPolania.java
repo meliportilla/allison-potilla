60d09a98031803216b01ea1fcd4c9767787177b9
camilap2022 <polasan1970@gmail.com>
2025-06-17 19:19:18 -0500

    Senatic, 11 puntos de códigos y tabla de la verdad.

---
+++ LauraCamilaPerdomoPolania.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lauracamilaperdomopolania;
+
+/**
+ *
+ * @author Camila
+ */
+public class LauraCamilaPerdomoPolania {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("1.Imprimir tu nombre y tu edad.");
+        String Name= "Laura Camila Perdomo Polania.";
+        String Age= "15.";
+        System.out.println("Mi nombre es : "+Name);
+        System.out.println("Mi edad es: "+Age);
+        
+        System.out.println("2. Calcular la suma de 2 numeros.");
+        int J=22;
+        int C=54;
+        int JC=J+C;
+        System.out.println("22+54="+JC);
+        System.out.println("3. Restar 2 numeros ingresados por el usuario.");
+        double Z=18.32;
+        double D=38.15;
+        System.out.println("18.32-38.15="+(Z-D));
+        System.out.println("4. Multiplicar 2 numeros y mostrar el resultado.");
+        int ca=41;
+        int to=4;
+        int pe=ca*to;
+        System.out.println("41*4="+pe);
+        System.out.println("5. Dividir 2 numeros y mostrar el cociente.");
+        int Sa=1767;
+        int Sh=12;
+        int K=Sa/Sh;
+        System.out.println("1767/12="+K);
+        System.out.println("6. Calcular el cuadrado de un numero.");
+        int Da=32;
+        int Perro=Da*Da;
+        System.out.println("32*32="+Perro);
+        System.out.println("7. Covertir grados Celsius a Fahrenheit.");
+        double Car=1.8;
+        int Cat=95;
+        int Dog=32;
+        System.out.println("(1.8*95)+32="+(Car*Cat)+Dog);
+        System.out.println("8. Calcular el perimetro de un rectangulo.");
+        int B=16;
+        int H=25;
+        int BH=2*(B+H);
+        System.out.println("2*(16+25)="+BH);
+        System.out.println("9. Calcular el area de un circulo.");
+        double Pi=3.14;
+        int r=3;
+        double rosa= (Pi*(r*r));
+        System.out.println("3.14*(3*3)="+rosa);
+        System.out.println("10. Convertit años a dias.");
+        int años=17;
+        int end=años*365;
+        System.out.println("17*365="+end);
+        System.out.println("11. Calcular el doble y el triple de un numero ingresado.");
+        int casa=59;
+        int doble=2*(casa);
+        int triple=3*(casa);
+        System.out.println("2*(59)="+doble+" doble del numero");
+        System.out.println("3*(59=)"+triple+" triple del numero");
+               
+        
+             
+        
+     
+        
+                
+        
+              
+         
+        
+        
+        
+   
+    }
+ 
+}
